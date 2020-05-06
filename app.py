314037aee8a001a79f90706d15e3af1e8b4612f5
mathews1193 <mathews1193@yahoo.com>
2020-05-05 21:15:55 -0500

    second commit

--- app.py
+++ app.py
@@ -3,4 +3,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1> Hello, Flask! <h1>"
+    return "<h1> Home Page <h1>"
+
+@app.route("/about")
+def about():
+    return "<h1> About <h1>"
+
