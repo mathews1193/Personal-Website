bbeb8b54cb75504a0fc5125c96de0cf1cceb3a5b
mathews1193 <mathews1193@yahoo.com>
2020-05-06 09:49:49 -0500

    third commit

--- app.py
+++ app.py
@@ -2,10 +2,24 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route('/home')
 def home():
     return "<h1> Home Page <h1>"
 
 @app.route("/about")
 def about():
     return "<h1> About <h1>"
+
+
+@app.route("/projects")
+def project():
+    return "<h1> Projects <h1>"
+
+
+@app.route("/contact")
+def contact():
+    return "<h1> Contact Information <h1>"
+
+
+
 
