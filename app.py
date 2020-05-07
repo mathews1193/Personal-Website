3dc612b95866ad0cb95fc3517b2175039d0f5726
mathews1193 <mathews1193@yahoo.com>
2020-05-06 19:47:10 -0500

    Created layout for webpages

--- app.py
+++ app.py
@@ -1,21 +1,39 @@
 from flask import Flask , render_template
 app = Flask(__name__)
 
-@app.route("/")
+posts = [
+    {
+        'author': 'Donald Mathews',
+        'title': 'Project 1 ', 
+        'content': 'First post content',
+        'date_posted': 'May 6, 2020'
+    },
+    {
+        'author': 'Donald Mathews',
+        'title': 'Project 2 ', 
+        'content': 'Second post content',
+        'date_posted': 'May 6, 2020'
+    }
+
+]
+
+# home page 
+@app.route("/") 
 @app.route('/home')
 def home():
     return render_template ('home.html')
 
+# about page 
 @app.route("/about")
 def about():
     return render_template('about.html')
 
-
+# project page 
 @app.route("/projects")
 def project():
-    return render_template('projects.html')
+    return render_template('projects.html', posts=posts)
 
-
+# contact page 
 @app.route("/contact")
 def contact():
     return render_template('contact.html')
